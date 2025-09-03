4c68a86314704b3dab7c19da36a2c7ec5d89f222
Lance <miguel2002622@gmail.com>
2025-09-03 14:39:17 +0800

    feat: initial commit, add home page, add MovieCard
    - added movie card to display a card with the poster and info of a movie
    - added a homepage to display movie
    - added a search button (template)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+// Contains the user interface of the homepage
+
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John  Wick", release_date: "2020" },
+    { id: 2, title: "Angry Birds", release_date: "2012" },
+    { id: 3, title: "Demon Slayer", release_date: "2025" },
+  ];
+
+  const handleSearch = () => {
+    alert(searchQuery);
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
